83e7b99eaf1825f53614a462b14a3bce0070b042
callum <callumcancreate@gmail.com>
2020-04-13 01:07:17 +1000

    add user model and controller

--- src/server/controllers/users.ts
+++ src/server/controllers/users.ts
@@ -1,6 +1,12 @@
 import { asyncCatchWrapper } from '../../utils';
-// import User from "../models/User";
+import User from '../models/User';
 // import NamedError from '../models/NamedError';
+
+export const register = asyncCatchWrapper(async (req, res) => {
+  const user = new User(req.body);
+  await user.save();
+  res.status(201).send({ user });
+});
 
 export const login = asyncCatchWrapper((req, res) => {
   res.send(req.body);
---
+++ src/server/models/User/User.ts
@@ -0,0 +1,5 @@
+import Resource from '../Resource';
+
+class User extends Resource {}
+
+export default User;
