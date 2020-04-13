60eae8a096d975685fa65166431c2e13e221c578
callum <callumcancreate@gmail.com>
2020-04-14 08:53:32 +1000

    optimize build

--- src/server/server.tsx
+++ src/server/server.tsx
@@ -1,15 +1,19 @@
 import express from 'express';
+import path from 'path';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
+import comp from 'express-static-gzip';
 import apiRouter from './routes/api';
 import errorHandler from './middleware/errorHandler';
 import renderApp from './middleware/renderApp';
 
 const server = express();
+const buildPath = path.join(__dirname, '../dist');
 
 server.use(cookieParser());
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
+server.use(comp(buildPath, { enableBrotli: true, orderPreference: ['br'] }));
 server.use('/api/v1', apiRouter);
 server.use(express.static('dist/public'));
 server.use(errorHandler);
