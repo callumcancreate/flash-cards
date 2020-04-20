62cbf05b62cd6e58968e92d27e4a7f722caf82e8
callum <callumcancreate@gmail.com>
2020-04-20 10:16:19 +1000

    add keyboard events and tab index to category list

--- src/client/components/CategoryList/CategoryListItem.tsx
+++ src/client/components/CategoryList/CategoryListItem.tsx
@@ -5,26 +5,47 @@ import './CategoryListItem.scss';
 interface Props {
   category: Category;
   node?: number;
+  isParentOpen?: boolean;
 }
 
-const CategoryListItem: React.FC<Props> = ({ category, node = 0 }) => {
+const CategoryListItem: React.FC<Props> = ({
+  isParentOpen = true,
+  category,
+  node = 0
+}) => {
   const [isOpen, setOpen] = useState(false);
+  const hasChildren = !!category.children.length;
   const onClick = (e) => {
     e.stopPropagation();
     setOpen(!isOpen);
   };
+  const onLabelPress = (e) => {
+    if (e.key === 'Enter') setOpen(!isOpen);
+  };
   return (
-    <div className="category-list-item" onClick={onClick}>
+    <div className="category-list-item">
       <div
         className="label"
+        onClick={onClick}
+        role="button"
+        tabIndex={0}
         style={{ backgroundColor: `rgba(100, 100, 100, ${0 + node / 10})` }}
+        onKeyDown={onLabelPress}
       >
         {category.name}
+        {hasChildren && (
+          <ion-icon name={`chevron-${isOpen ? 'up' : 'down'}-outline`} />
+        )}
       </div>
-      <div className={`children${isOpen ? ' open' : ''}`}>
+      <div className={`children${isOpen && isParentOpen ? ' open' : ''}`}>
         {category.children &&
           category.children.map((c) => (
-            <CategoryListItem key={c.categoryId} category={c} node={node + 1} />
+            <CategoryListItem
+              key={c.categoryId}
+              category={c}
+              node={node + 1}
+              isParentOpen={isOpen}
+            />
           ))}
       </div>
     </div>
